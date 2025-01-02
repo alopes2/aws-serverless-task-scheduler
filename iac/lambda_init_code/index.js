507a9a20e48387ac5f362ddc26dfd86c9b542cee
Andre <andrevitorlopes@gmail.com>
2025-01-02 15:07:47 +0100

    Fix lambda default code

--- iac/lambda_init_code/index.js
+++ iac/lambda_init_code/index.js
@@ -1,5 +1,5 @@
-﻿// Default handler generated in AWS
-export const handler = async (event) => {
+// Default handler generated in AWS
+exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
